a8a2e6613259dd35a0737d7c47bef8bcd1d01d82
gustavo michels maykot <gustavom1308@gmail.com>
2024-05-14 02:09:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+        const xingamentos = ["viado","baitola","puto","esquizofrenico","bosta","lixo","resto de aborto","fudido","arrombado","cuzao","limitado","retardado","doente","animal","energumeno","babaca","quadrupede","mazanza","mama rola","burro","jumento"];
+        const id = document.getElementById('xingamento');
+       let i=0;
+        function mudarXingamento(){
+            id.textContent=xingamentos[i].toUpperCase();
+            i = (i+1)%xingamentos.length;
+
+        }
+        setInterval(mudarXingamento,1000);
+
+});
